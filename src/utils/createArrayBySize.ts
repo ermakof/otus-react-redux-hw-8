843d0f7540f31ac00733b1b64fd22bbbfda8d1ee
Ermakov.Alexey <Ermakov.Alexey@russianpost.ru>
2022-01-18 20:07:07 +0300

    [main] clone project otus-react-redux-7 into current project, create README.md

---
+++ src/utils/createArrayBySize.ts
@@ -0,0 +1,4 @@
+const createIndexArrayBySize = (size: number = 0) =>
+  Object.keys([...Array(size)]).map((v) => parseInt(v));
+
+export default createIndexArrayBySize;
---
+++ src/utils/createGameField.ts
@@ -0,0 +1,18 @@
+import { compose } from 'ramda';
+import createIndexArrayBySize from '@src/utils/createArrayBySize';
+import shuffle from '@src/utils/shuffle';
+
+const generateCells = (fillPercent: number) => (randomArray: Array<number>) => {
+  let countFilledCells = parseInt(((randomArray.length * fillPercent) / 100).toFixed(), 10);
+  const rez = randomArray.map(() => 0);
+  for (let i = 0; i < countFilledCells; i = i + 1) {
+    rez[randomArray[i]] = 1;
+  }
+  return rez;
+};
+
+const createGameField = (size: number, fillPercent: number = 30) => {
+  return compose(generateCells(fillPercent), shuffle, createIndexArrayBySize)(size);
+};
+
+export default createGameField;
